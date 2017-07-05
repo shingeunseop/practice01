211140aab347aacafd48f8bf8c49455a77c0b916
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 14:01:36 +0900

    7/05 practice 04

---
+++ src/com/javaex/practice/Prac04.java
@@ -0,0 +1,17 @@
+package com.javaex.practice;
+
+public class Prac04 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) {
+			for(int j=0;j<=9;j++) {
+				System.out.print(i+j);
+				System.out.print("\t");
+				
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
